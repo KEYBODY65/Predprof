8ed0a3526b998b27782abcdaa1184df371e36e85
Misha <popov27051971@gmail.com>
2024-03-30 09:45:34 +0300

    Task 1

---
+++ first.py
@@ -0,0 +1,23 @@
+with open('space.csv', 'r', encoding='UTF-8') as file_input:
+    data = {}
+    for elem in file_input.readlines()[1:]:
+        process = elem.split(',')
+        if '0 0' in process[2]:
+            koords = process[-1]
+            data.setdefault(process[0], [process[1], process[-1].rstrip()])
+
+with open('space_new.txt', 'w', encoding='UTF-8') as file_write:
+    for k, v in data.items():
+        s_x = 0
+        s_y = 0
+        if '-' == v[-1][0]:
+            x = int(v[-1][1]) * -1
+            s_x = len(v[0]) - x
+        elif '-' == v[-1][-2]:
+            y = int(v[-1]) * -1
+            s_y = len(v[0]) - y
+        else:
+            s_x = len(v[0]) - int(v[-1][0])
+            s_y = len(v[0]) - int(v[-1][-1])
+        file_write.write(
+            f"При получении данных о корабле {k} возникли сбои. Предположительные координаты - {s_x},{s_y}" + '\n')
